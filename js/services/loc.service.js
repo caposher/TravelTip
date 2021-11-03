de681391a939d12b034502daa3e72184294cbe86
capos <caposher@gmail.com>
2021-11-03 14:43:28 +0200

    change Locs variable

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,25 @@
 export const locService = {
-    getLocs
-}
-
+  getLocs,
+};
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
+  {
+    id: 1,
+    name: 'Greatplace',
+    lat: 32.047104,
+    lng: 34.832384,
+    weather: 1,
+    createdAt: Date.now(),
+    updatedAt: Date.now() - 1000,
+  },
+
+  //   { id: 2, name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
+];
 
 function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000)
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(locs);
+    }, 2000);
+  });
 }
-
-
