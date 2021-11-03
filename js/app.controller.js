5cdca6558bb616147ed6dc99f00301ff375be687
AnnaM317 <anya317@gmail.com>
2021-11-03 18:48:57 +0200

    added weather service

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,16 +1,17 @@
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
+import { weatherService } from './services/weather.service.js'
 
-window.onload = onInit;
-window.onAddMarker = onAddMarker;
-window.onPanTo = onPanTo;
-window.onGetLocs = onGetLocs;
-window.onGetUserPos = onGetUserPos;
+// window.onload = onInit;
+// window.onAddMarker = onAddMarker;
+// window.onPanTo = onPanTo;
+// window.onGetLocs = onGetLocs;
+// window.onGetUserPos = onGetUserPos;
 
-window.onSearch = onSearch;
-window.onMyLocation = onMyLocation;
-window.onDeleteLocation = onDeleteLocation;
-window.onChangeLocation = onChangeLocation;
+// window.onSearch = onSearch;
+// window.onMyLocation = onMyLocation;
+// window.onDeleteLocation = onDeleteLocation;
+// window.onChangeLocation = onChangeLocation;
 
 function onInit() {
     mapService.initMap()
@@ -22,7 +23,7 @@ function onInit() {
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-    console.log('Getting Pos');
+    console.log("Getting Pos");
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
@@ -55,4 +56,38 @@ function onGetUserPos() {
 function onPanTo() {
     console.log('Panning the Map');
     mapService.panTo(35.6895, 139.6917);
+}
+
+var coords={
+    lat:32.0749831,
+    lng:34.9120554
+};
+
+onGetWeather(coords);
+
+function onGetWeather(coords){
+    weatherService.getWeather(coords)
+    .then(weather => renderWeather(weather));
+}
+
+function renderWeather(currWeather){
+    // var currWeather = {
+        // city: weather.data.name,
+        // temp: weather.data.main.temp,
+        // tempMin:weather.data.main.temp_min,
+        // tempMax: weather.data.main.temp_max,
+        // weatherDesc:weather.data.weather[0].description,
+        // windSpeed: weather.data.wind.speed,
+        // icon: `http://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`
+    var elWeather = document.querySelector('.weather-container');
+    const strHtmls = `
+        <p>City: ${currWeather.city}</p>
+        <p class="weather-desc"><img class="weather-icon" src="${currWeather.icon}"/></p>
+        <p> ${currWeather.weatherDesc}</p>
+        <p>Current Temp ${currWeather.temp}°C</p>
+        <p>Temperature: from ${currWeather.tempMin}°C to ${currWeather.tempMax}°C</p>
+        <p>Wind: ${currWeather.windSpeed}m/s</p>
+        `;
+
+    elWeather.innerHTML = strHtmls;
 }
